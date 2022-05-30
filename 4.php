8db0fc0c2bb1bf4967c6b340842ef5fd9f5d8827
drmcastles <drmcast@yandex.ru>
2022-05-30 17:59:08 +0300

    second commit

--- 4.php
+++
@@ -1,6 +0,0 @@
-<?php
-$iHardLinkToAnotherVariable =& $iTwentyFive;
-$iTwentyFive = 25;
-$iHardLinkToAnotherVariable = 30;
-
-echo "TwentyFive равна ".$iTwentyFive."<br>"."переменная iHardLinkToAnotherVariable равна"."$iHardLinkToAnotherVariable";
